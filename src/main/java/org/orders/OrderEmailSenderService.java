116cad6f90a01ce128478ae5639122262002b040
oleksandrkharchenko <deepalexkh@gmail.com>
2023-01-08 08:40:06 +0100

    clearance email sender service

--- src/main/java/org/orders/OrderEmailSenderService.java
+++ src/main/java/org/orders/OrderEmailSenderService.java
@@ -14,25 +14,15 @@ public class OrderEmailSenderService {
 
     public static void sendMailTo(String mailTo, String redeemCode){
 
-    // Recipient's email ID needs to be mentioned.
     String to = mailTo;
-
-    // Sender's email ID needs to be mentioned
     String from = "s15638@pjwstk.edu.pl";
-
-    // Assuming you are sending email from through gmails smtp
     String host = "smtp.gmail.com";
-
-    // Get system properties
     Properties properties = System.getProperties();
-
-    // Setup mail server
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.ssl.enable", "true");
         properties.put("mail.smtp.auth", "true");
 
-    // Get the Session object.// and pass username and password
     Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
         protected PasswordAuthentication getPasswordAuthentication() {
@@ -43,31 +33,17 @@ public class OrderEmailSenderService {
 
     });
 
-    // Used to debug SMTP issues
-//        session.setDebug(true);
-
         try {
-        // Create a default MimeMessage object.
         MimeMessage message = new MimeMessage(session);
-
-        // Set From: header field of the header.
         message.setFrom(new InternetAddress(from));
-
-        // Set To: header field of the header.
         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-        // Set Subject: header field
         message.setSubject("InetItemStore. Order completed.");
-
-        // Now set the actual message
         message.setText("This is your code: " + redeemCode);
-
-        System.out.println("sending...");
-        // Send message
+        System.out.println("sending email...");
         Transport.send(message);
-        System.out.println("Sent message successfully....");
-    } catch (MessagingException mex) {
-        mex.printStackTrace();
+        System.out.println("Sent email successfully....");
+    } catch (MessagingException me) {
+        me.printStackTrace();
     }
 
     }
